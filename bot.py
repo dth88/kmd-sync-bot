200fc9abb5243a2169d063f675331f81584e047e
dathbezumniy <gdath100500@gmail.com>
2020-03-25 08:39:30 +0700

    almost finished with configuration setup...

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler, DictPersistence)
 
 
+#states
 CONFIGURE, CHOOSING_SERVER, ISSUING_COMMANDS = range(3)
 
 
@@ -15,7 +16,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 configure_keyboard = [['server_name', 'ipaddr'],
-                      ['root_pass', 'all_together']]
+                      ['root_pass', 'all_together', 'done']]
 reply_markup = ReplyKeyboardMarkup(configure_keyboard, one_time_keyboard=True)
 
 
@@ -31,7 +32,8 @@ def main():
             CONFIGURE: [MessageHandler(Filters.regex('^(server_name)$'), configure_name),
                         MessageHandler(Filters.regex('^(ipaddr)$'), configure_ip),
                         MessageHandler(Filters.regex('^(root_pass)$'), configure_root_pass),
-                        MessageHandler(Filters.regex('^(all_together)$'), configure)],
+                        MessageHandler(Filters.regex('^(all_together)$'), configure_all_together),
+                        MessageHandler(Filters.regex('^(done)$'), configure)],
 
             CHOOSING_SERVER: [MessageHandler(Filters.text, make_a_choice)],
 
@@ -45,23 +47,23 @@ def main():
     dp.add_error_handler(error)
 
 
-
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("start_sync", start_sync))
-    dp.add_handler(CommandHandler("start_sync_all", start_sync_all))
-    dp.add_handler(CommandHandler("stop_sync", stop_sync))
-    dp.add_handler(CommandHandler("stop_sync_all", stop_sync_all))
-    dp.add_handler(CommandHandler("setup_binary", setup_binary))
-    dp.add_handler(CommandHandler("get_current_sync_status", get_current_sync_status))
-    dp.add_handler(CommandHandler("help", help))
+    #dp.add_handler(CommandHandler("start", start))
+    #dp.add_handler(CommandHandler("start_sync", start_sync))
+    #dp.add_handler(CommandHandler("start_sync_all", start_sync_all))
+    #dp.add_handler(CommandHandler("stop_sync", stop_sync))
+    #dp.add_handler(CommandHandler("stop_sync_all", stop_sync_all))
+    #dp.add_handler(CommandHandler("setup_binary", setup_binary))
+    #dp.add_handler(CommandHandler("get_current_sync_status", get_current_sync_status))
+    #dp.add_handler(CommandHandler("help", help))
 
     updater.start_polling()
     updater.idle()
 
 
+@send_typing_action
 def start(update, context):
-    update.message.reply_text('Hi! Lets configure a new sync server!', reply_markup=configure_keyboard)
+    update.message.reply_text('Hi! Lets configure a new sync server! Just pick an option and provide the data, press done after you finished.',
+                              reply_markup=configure_keyboard)
     try:
         if context.user_data['servers']:
             pass
@@ -73,6 +75,7 @@ def start(update, context):
     return CONFIGURE
 
 
+@send_typing_action
 def configure_name(update, context):
     update.message.reply_text('Name your new sync-api server:', reply_markup=configure_keyboard)
     context.user_data['new_server']['name'] = update.message.text
@@ -81,6 +84,7 @@ def configure_name(update, context):
     return CONFIGURE
 
 
+@send_typing_action
 def configure_ip(update, context):
     update.message.reply_text('Please provide ip address of the server:', reply_markup=configure_keyboard)
     context.user_data['new_server']['ip'] = update.message.text
@@ -89,28 +93,46 @@ def configure_ip(update, context):
     return CONFIGURE
 
 
+@send_typing_action
 def configure_root_pass(update, context):
     update.message.reply_text('Now, root passsword please:', reply_markup=configure_keyboard)
-    context.user_data['new_server'] += '{}:'.format(update.message.text)
+    context.user_data['new_server']['rootpass'] = update.message.text
 
+
+    return CONFIGURE
+
+
+@send_typing_action
+def configure_all_together(update, context):
+    update.message.reply_text('format: name,ip,rootpassword')
+
+
+    return CONFIGURE
+
+
+@send_typing_action
+def configure(update, context):
+    new_server = context.user_data['new_server']
+    #first setup and if success then add to the 'servers'
+    context.user_data['servers'].append(new_server)
+    
 
     return CHOOSING_SERVER
 
 
-def configure(update, context):
-    update.message.reply_text('format: name,ip,rootpassword')
-
-
-
-
-
+@send_typing_action
 def make_a_choice(update, context):
-    update.message.reply_text('And the root passsword please:')
-    
+    available_servers = context.user_data['servers']
+    if len(available_servers) > 1:
+        pass
+    else:
+        update.message.reply_text('')
+
 
     return ISSUING_COMMANDS
 
 
+@send_typing_action
 def get_current_sync_status(update, context):
     msg = requests.get('http://95.217.134.179/sync_stats_all').json()
     amount = int(msg['amount'])
@@ -123,6 +145,7 @@ def get_current_sync_status(update, context):
     update.message.reply_text(reply)
 
 
+@send_typing_action
 def setup_binary(update, context):
     """Send a message when the command /start_sync is issued."""
     link = {'link' : context.args[0]}
@@ -130,6 +153,7 @@ def setup_binary(update, context):
     update.message.reply_text(msg)
 
 
+@send_typing_action
 def start_sync(update, context):
     """Send a message when the command /start_sync is issued."""
     for ticker in context.args:
@@ -137,6 +161,7 @@ def start_sync(update, context):
         update.message.reply_text(msg)
 
 
+@send_typing_action
 def stop_sync(update, context):
     """Send a message when the command /start_sync is issued."""
 
@@ -145,6 +170,7 @@ def stop_sync(update, context):
         update.message.reply_text(msg)
 
 
+@send_typing_action
 def start_sync_all(update, context):
     """Send a message when the command /start_sync is issued."""
 
@@ -152,6 +178,7 @@ def start_sync_all(update, context):
     update.message.reply_text(msg)
 
 
+@send_typing_action
 def stop_sync_all(update, context):
     """Send a message when the command /start_sync is issued."""
 
@@ -163,6 +190,7 @@ def stop_sync_all(update, context):
     update.message.reply_text(msg)
 
 
+@send_typing_action
 def help(update, context):
     """Send a message when the command /help is issued."""
     help_msg  = 'Configuration:\n'
