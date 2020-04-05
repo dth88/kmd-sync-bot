f4da2ee10188ed646560fce8e8da3ca20b046cdd
dathbezumniy <gdath100500@gmail.com>
2020-04-05 19:32:29 +0700

    table formatting

--- bot.py
+++ bot.py
@@ -208,7 +208,7 @@ def get_current_sync_status(update, context):
     amount = int(msg['amount'])
     stats = msg['stats']
     reply = '<pre>Currently {} assetchains are syncing:\n'.format(amount)
-    reply += '{:7}| {:5}|  {:5}|  {:7}|  {:3}\n'.format('TICKER', 'SYNC', 'GOT', 'TOTAL', '%')
+    reply += 'TICKER | SYNC |  GOT   |  TOTAL  |  %\n'
     
     if amount:
         for k,v in stats.items():
